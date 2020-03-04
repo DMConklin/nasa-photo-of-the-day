dd240d876b0c808fd6702a19ec137650508bb3dc
Darrell-Conklin <dmconklin84@gmail.com>
2020-03-03 22:47:42 -0600

    passed and rendered API data

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from 'axios';
 import "./App.css";
 import Header from "./components/Header/Header.js";
 import Media from "./components/MediaContainer/MediaContainer.js";
 
 function App() {
+  const [data, setData] = useState(Object);
 
-useEffect(() => {
-   axios
-    .get("https://api.nasa.gov/planetary/apod?api_key=VSDX406ElKtT7Zaql3PPtpmCz7yhy6tKEgRV796g")
-    .then(response => console.log(response));
-}, []);
+  useEffect(() => {
+    axios
+      .get("https://api.nasa.gov/planetary/apod?api_key=VSDX406ElKtT7Zaql3PPtpmCz7yhy6tKEgRV796g")
+      .then(response => {
+        setData(response.data);
+      });
+  }, []);
 
   return (
     <div className="App">
       <Header />
-      <Media />
+      <Media date={data.date} explanation={data.explanation} media_type={data.media_type} title={data.title} url={data.url} />
     </div>
   );
 }
--- src/components/MediaContainer/MediaContainer.js
+++ src/components/MediaContainer/MediaContainer.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const MediaContainer = () => {
+const MediaContainer = (props) => {
+    const [media, setMedia] = useState();
+    useEffect(() => {
+        if (props.media_type === 'video') {
+            setMedia(
+                <iframe 
+                    width="560" 
+                    height="315" 
+                    src={props.url} 
+                    frameBorder="0" 
+                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+                    allowFullScreen
+                    title={props.title}>
+                </iframe>
+            )
+        } else if (props.media_type === 'image') {
+            setMedia(
+                <img src={props.url} alt={props.title} />
+            )
+        }
+    }, [props])
     return(
         <div className="media-container">
-
+            <h1>{props.title}</h1>
+            {media}
+            <p>{props.explanation}</p>
         </div>
     )
 }
